cc4aa46074e9589b0ca187ce15bcb38446df4c4f
Djonathan Barros <djonathan.barros@gmail.com>
2021-09-20 22:40:20 -0300

    projeto inicializado

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "unipar-2021-trabalho-plano"
---
+++ src/main/kotlin/br/unipar/plano/Unipar2021TrabalhoPlanoApplication.kt
@@ -0,0 +1,11 @@
+package br.unipar.plano
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Unipar2021TrabalhoPlanoApplication
+
+fun main(args: Array<String>) {
+    runApplication<Unipar2021TrabalhoPlanoApplication>(*args)
+}
