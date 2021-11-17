c3bcda02372e7b1c8c3334f83a830db7cd611065
marianistefanoj <77533938+marianistefanoj@users.noreply.github.com>
2021-11-17 00:34:29 -0300

    teste

--- src/main/kotlin/br/unipar/plano/domain/credenciamentos/services/PrestMedQueryService.kt
+++ src/main/kotlin/br/unipar/plano/domain/credenciamentos/services/PrestMedQueryService.kt
@@ -1,4 +1,12 @@
 package br.unipar.plano.domain.credenciamentos.services
 
+import br.unipar.plano.domain.centrais.model.Central
+import br.unipar.plano.domain.centrais.model.IdCentral
+import br.unipar.plano.domain.credenciamentos.model.IdPrestadorMedico
+import br.unipar.plano.domain.credenciamentos.model.PrestadorMedico
+import br.unipar.plano.interfaces.rest.credenciamentos.PrestMedDTO
+
 interface PrestMedQueryService {
+    fun lista(): List<PrestMedDTO>
+    fun buscaPorId(idPrestadorMedico: IdPrestadorMedico): PrestadorMedico
 }
