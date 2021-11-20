991eb62a99c28327c5283f4b468929c9241037ca
Djonathan Barros <djonathan.barros@gmail.com>
2021-11-19 22:52:00 -0300

    refatorados DTOs para fazer tradução para os modelos

--- src/main/kotlin/br/unipar/plano/domain/centrais/services/impl/CentralApplicationServiceImpl.kt
+++ src/main/kotlin/br/unipar/plano/domain/centrais/services/impl/CentralApplicationServiceImpl.kt
@@ -1,7 +1,5 @@
 package br.unipar.plano.domain.centrais.services.impl
 
-import br.unipar.plano.domain.centrais.model.Central
-import br.unipar.plano.domain.centrais.model.Endereco
 import br.unipar.plano.domain.centrais.model.IdCentral
 import br.unipar.plano.domain.centrais.services.CentralApplicationService
 import br.unipar.plano.domain.centrais.services.CentralQueryService
@@ -9,7 +7,6 @@ import br.unipar.plano.domain.centrais.usecases.CriaCentralUseCase
 import br.unipar.plano.interfaces.rest.centrais.CentralDTO
 import br.unipar.plano.interfaces.rest.centrais.CentralDetailsDTO
 import br.unipar.plano.interfaces.rest.centrais.CentralSummaryDTO
-import br.unipar.plano.interfaces.rest.centrais.EnderecoDTO
 import org.springframework.stereotype.Service
 
 @Service
@@ -19,54 +16,16 @@ class CentralApplicationServiceImpl(
 ) : CentralApplicationService {
 
     override fun cria(centralDTO: CentralDTO): IdCentral {
-        val central = toModel(IdCentral(), centralDTO)
+        val central = centralDTO.toModel(IdCentral(), centralDTO)
         val novaCentral = criaCentralUseCase.executa(central)
         return novaCentral.id
     }
 
-    override fun lista(): List<CentralSummaryDTO> = centralQueryService.lista().map {
-        toSummaryDTO(it)
+    override fun lista() = centralQueryService.lista().map(CentralSummaryDTO::toDTO)
+
+    override fun buscaPorId(idCentral: IdCentral): CentralDetailsDTO {
+        val central = centralQueryService.buscaPorId(idCentral)
+        return CentralDetailsDTO.toDTO(central)
     }
-
-    override fun buscaPorId(idCentral: IdCentral): CentralDetailsDTO = toDetailsDTO(centralQueryService.buscaPorId(idCentral))
-
-    private fun toModel(id: IdCentral, centralDTO: CentralDTO) = Central(
-        id = id,
-        nome = centralDTO.nome,
-        cnpj = centralDTO.cnpj,
-        endereco = Endereco(
-            idCentral = id,
-            cidade = centralDTO.endereco.cidade,
-            cep = centralDTO.endereco.cep,
-            bairro = centralDTO.endereco.bairro,
-            logradouro = centralDTO.endereco.logradouro,
-            numero = centralDTO.endereco.numero,
-            complemento = centralDTO.endereco.complemento
-        )
-    )
-
-    private fun toDetailsDTO(central: Central) = CentralDetailsDTO(
-        id = central.id,
-        centralData = toDTO(central)
-    )
-
-    private fun toDTO(central: Central) = CentralDTO(
-        nome = central.nome,
-        cnpj = central.cnpj,
-        endereco = EnderecoDTO(
-            cidade = central.endereco.cidade,
-            cep = central.endereco.cep,
-            bairro = central.endereco.bairro,
-            logradouro = central.endereco.logradouro,
-            numero = central.endereco.numero,
-            complemento = central.endereco.complemento
-        )
-    )
-
-    private fun toSummaryDTO(central: Central) = CentralSummaryDTO(
-        id = central.id,
-        nome = central.nome,
-        cidade = central.endereco.cidade,
-    )
 
 }
--- src/main/kotlin/br/unipar/plano/interfaces/rest/centrais/CentralDTO.kt
+++ src/main/kotlin/br/unipar/plano/interfaces/rest/centrais/CentralDTO.kt
@@ -1,5 +1,7 @@
 package br.unipar.plano.interfaces.rest.centrais
 
+import br.unipar.plano.domain.centrais.model.Central
+import br.unipar.plano.domain.centrais.model.Endereco
 import br.unipar.plano.domain.centrais.model.IdCentral
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
@@ -12,12 +14,35 @@ data class CentralSummaryDTO(
     val id: IdCentral,
     val nome: String,
     val cidade: Int
-)
+) {
+
+    companion object {
+
+        fun toDTO(central: Central) = CentralSummaryDTO(
+            id = central.id,
+            nome = central.nome,
+            cidade = central.endereco.cidade,
+        )
+
+    }
+
+}
 
 data class CentralDetailsDTO(
     val id: IdCentral,
     val centralData: CentralDTO
-)
+) {
+
+    companion object {
+
+        fun toDTO(central: Central) = CentralDetailsDTO(
+            id = central.id,
+            centralData = CentralDTO.toDTO(central)
+        )
+
+    }
+
+}
 
 data class CentralDTO(
 
@@ -35,7 +60,41 @@ data class CentralDTO(
     @field:NotNull
     val endereco: EnderecoDTO
 
-)
+) {
+
+    fun toModel(id: IdCentral, centralDTO: CentralDTO) = Central(
+        id = id,
+        nome = centralDTO.nome,
+        cnpj = centralDTO.cnpj,
+        endereco = Endereco(
+            idCentral = id,
+            cidade = centralDTO.endereco.cidade,
+            cep = centralDTO.endereco.cep,
+            bairro = centralDTO.endereco.bairro,
+            logradouro = centralDTO.endereco.logradouro,
+            numero = centralDTO.endereco.numero,
+            complemento = centralDTO.endereco.complemento
+        )
+    )
+
+    companion object {
+
+        fun toDTO(central: Central) = CentralDTO(
+            nome = central.nome,
+            cnpj = central.cnpj,
+            endereco = EnderecoDTO(
+                cidade = central.endereco.cidade,
+                cep = central.endereco.cep,
+                bairro = central.endereco.bairro,
+                logradouro = central.endereco.logradouro,
+                numero = central.endereco.numero,
+                complemento = central.endereco.complemento
+            )
+        )
+
+    }
+
+}
 
 data class EnderecoDTO(
 
