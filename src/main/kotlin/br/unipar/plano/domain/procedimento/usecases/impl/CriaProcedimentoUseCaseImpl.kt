242ad21df6dd9137a0437cb49283250ea318082d
Henrique Maltauro <henrique.maltauro.dev@gmail.com>
2021-12-06 15:26:36 -0300

    Criando gravação do procedimento.

--- src/main/kotlin/br/unipar/plano/domain/procedimento/usecases/impl/CriaProcedimentoUseCaseImpl.kt
+++ src/main/kotlin/br/unipar/plano/domain/procedimento/usecases/impl/CriaProcedimentoUseCaseImpl.kt
@@ -8,21 +8,25 @@ import br.unipar.plano.domain.centrais.usecases.CriaCentralUseCase
 import br.unipar.plano.domain.procedimento.model.Especialidade
 import br.unipar.plano.domain.procedimento.model.Procedimento
 import br.unipar.plano.domain.procedimento.model.ProcedimentoRepository
+import br.unipar.plano.domain.procedimento.services.ProcedimentoQueryService
 import br.unipar.plano.domain.procedimento.usecases.CriaProcedimentoUseCase
+import br.unipar.plano.domain.solicitacaoprocedimento.service.SolicitacaoProcedimentoQueryService
 import org.springframework.stereotype.Service
 
 @Service
-class CriaProcedimentoUseCaseImpl(private val procedimentoRepository: ProcedimentoRepository) : CriaProcedimentoUseCase {
+class CriaProcedimentoUseCaseImpl(private val procedimentoRepository: ProcedimentoRepository,
+                                  private val solicitacaoProcedimentoQueryService: SolicitacaoProcedimentoQueryService) : CriaProcedimentoUseCase {
 
     override fun executa(procedimento: Procedimento): Procedimento {
         if (procedimentoRepository.existsById(procedimento.id))
             throw IllegalStateException("Outro procedimento com id ${procedimento.id} já foi cadastrado!")
 
         if (!procedimento.carteirinha.status.equals(StatusCarteirinha.VALIDA))
-            throw IllegalStateException("A carteirinha com id ${procedimento.carteirinha.id} não é válida!")
+            throw IllegalStateException("A carteirinha com id ${procedimento.carteirinha.numeroCarteirinha} não é válida!")
 
         if (!procedimento.especialidade.equals(Especialidade.CLINICA_MEDICA))
-            throw IllegalStateException("O procedimento com id ${procedimento.id} deve estar atrelado a uma liberação!")
+            if (solicitacaoProcedimentoQueryService.findByProcedimento_Id(procedimento.id).count() == 0)
+                throw IllegalStateException("O procedimento com id ${procedimento.id} deve estar atrelado a uma liberação!")
 
         return procedimentoRepository.save(procedimento)
     }
