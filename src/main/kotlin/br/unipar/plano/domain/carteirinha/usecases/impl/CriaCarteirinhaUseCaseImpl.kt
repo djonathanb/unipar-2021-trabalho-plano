6499d5259e2d01c2813d34632711d414f30cef15
Adriel Schneider <75347807+AdrielSchneider@users.noreply.github.com>
2021-12-06 13:55:39 -0300

    Criadas exceções
    Criadas exceções

--- src/main/kotlin/br/unipar/plano/domain/carteirinha/usecases/impl/CriaCarteirinhaUseCaseImpl.kt
+++ src/main/kotlin/br/unipar/plano/domain/carteirinha/usecases/impl/CriaCarteirinhaUseCaseImpl.kt
@@ -1,23 +1,26 @@
 package br.unipar.plano.domain.carteirinha.usecases.impl
 
+import br.unipar.plano.application.exceptions.CarteirinhaCadastradaException
+import br.unipar.plano.application.exceptions.CarteirinhaUsuarioCadastradaException
 import br.unipar.plano.domain.carteirinha.model.Carteirinha
 import br.unipar.plano.domain.carteirinha.model.CarteirinhaRepository
 import br.unipar.plano.domain.carteirinha.usecases.CriaCarteirinhaUseCase
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class CriaCarteirinhaUseCaseImpl(private val carteirinhaRepository: CarteirinhaRepository): CriaCarteirinhaUseCase {
     override fun cria(carteirinha: Carteirinha): Carteirinha {
 
-        var carteirinhaResult = carteirinhaRepository.findCarteirinhaValidaByIdUSuario(carteirinha.idUsuario)
+        var carteirinhaResult: Optional<Carteirinha> = carteirinhaRepository.findCarteirinhaValidaByIdUSuario(carteirinha.idUsuario)
 
         if (carteirinhaResult.isPresent)
-            throw Exception("Usuário ${carteirinha.idUsuario} já possui uma carteirinha válida: ${carteirinhaResult.get().numeroCarteirinha}")
+            throw CarteirinhaUsuarioCadastradaException(carteirinhaResult.get().numeroCarteirinha, carteirinha.idUsuario)
 
         carteirinhaResult = carteirinhaRepository.findById(carteirinha.numeroCarteirinha)
 
         if (carteirinhaResult.isPresent)
-            throw Exception("A carteirinha ${carteirinha.numeroCarteirinha} já está cadastrada")
+            throw CarteirinhaCadastradaException(carteirinha.numeroCarteirinha)
 
         return carteirinhaRepository.save(carteirinha)
     }
